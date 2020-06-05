6db29ab8d6a25c7cdb014afa103462d2ba5960bb
Joe Krall <kralljr@outlook.com>
2020-06-05 11:55:58 -0400

    added link functionality and changed header padding

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Header = () => (
   <nav>
+  <Link to='/' className="header-link">
     <h1 className= "header">Reciplease</h1>
+  </Link>
   </nav>
 )
 
