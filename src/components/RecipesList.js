e4df112994bd078ecd0fe380f8e6faac18716829
Joe Krall <kralljr@outlook.com>
2020-06-04 22:16:53 -0400

    added basic styling to Main View Recipe. Needs work!

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -7,19 +7,20 @@ const RecipesList = ({ recipes }) => (
 
   <div className="recipes-container">
     <h1>Recipes</h1>
-    <ol className="list-group">
+    <div className="list-group ">
       {
         recipes.map(recipe => (
-          <li className="list-group-item recipe" key={recipe.id}>
+          <div className="list-group-item recipe container d-inline-flex flex-wrap mb-3 p-3 border rounded side-view shadow" key={recipe.id}>
+
             <Link to={`/recipes/${recipe.id}`}>
+              <img src={image_url_start + recipe.image} className="img-fluid img-thumbnail" alt="Responsive image"/>
               <h3>{recipe.title}</h3>
-              <img width="200px" src={image_url_start + recipe.image}/>
               <h5>Ready in {recipe.readyInMinutes} minutes</h5>
             </Link>
-          </li>
+          </div>
         ))
       }
-    </ol>
+    </div>
   </div>
 )
 
