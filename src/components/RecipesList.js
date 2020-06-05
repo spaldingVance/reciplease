e97037c47186876e18a4b55c3ad7c3d372cf722e
Spalding Vance <spaldingvance@Spaldings-MacBook-Pro.local>
2020-06-05 11:15:15 -0400

    fixed styling and formatting

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -6,20 +6,19 @@ const image_url_start = "https://spoonacular.com/recipeImages/"
 const RecipesList = ({ recipes }) => (
 
   <div className="recipes-container">
-    <h1>Recipes</h1>
     <div className="list-group ">
       {
         recipes.map(recipe => (
-          <div className="list-group-item recipe container d-inline-flex flex-wrap mb-3 p-3 border rounded side-view shadow" key={recipe.id}>
+            <div className="list-group-item recipe container flex-wrap mb-3 p-3 border rounded side-view shadow" key={recipe.id}>
 
-            <Link to={`/recipes/${recipe.id}`}>
-
-              <h3>{recipe.title}</h3>
-              <div className="image-scaling">
-              <img src={image_url_start + recipe.image} className="img-fluid img-thumbnail mb-2 mx-auto d-block" alt="Responsive image"/><h5>Ready in {recipe.readyInMinutes} minutes</h5>
-              </div>
-            </Link>
-          </div>
+              <Link to={`/recipes/${recipe.id}`}>
+                <h3>{recipe.title}</h3>
+                <div className="image-scaling">
+                  <img src={image_url_start + recipe.image} className="img-fluid img-thumbnail mb-2 mx-auto d-block" alt="Responsive image"/>
+                  <h5>Ready in {recipe.readyInMinutes} minutes</h5>
+                </div>
+              </Link>
+            </div>
         ))
       }
     </div>
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -15,7 +15,7 @@ class SearchBar extends Component {
   render() {
     return (
       <form className="form d-inline shadow-sm">
-        <div className="input-group mt-3 mb-3 ">
+        <div className="input-group mb-3 ">
           <div className="input-group-prepend ">
             <div className="input-group-btn">
               <button type="button" className="btn btn-primary search-button shadow-sm" onClick={this.handleClick}>
@@ -23,7 +23,7 @@ class SearchBar extends Component {
                   </button>
                 </div>
               </div>
-              <input className="input-group form-control ml-sm-2 search-bar shadow-sm" value={this.state.term} onChange={this.onInputChange} />
+              <input className="input-group form-control ml-sm-2 search-bar shadow-sm" value={this.state.term} onChange={this.onInputChange} placeholder="find your recipe"/>
           </div>
         </form>
     );
