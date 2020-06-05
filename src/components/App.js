9832fd479d63c21e37682e95f255f7427e25bdfb
Kyle Stefano <kstefano327@gmail.com>
2020-06-05 10:27:20 -0400

    increasing results to 10

--- src/components/App.js
+++ src/components/App.js
@@ -19,49 +19,94 @@ class App extends Component {
     this.state = {
       recipes: [
         {
-          "id": 495020,
-          "title": "Pumpkin Spice Breakfast Shake",
-          "readyInMinutes": 10,
-          "servings": 1,
-          "sourceUrl": "http://ohmyveggies.com/recipe-pumpkin-spice-breakfast-shake/",
-          "openLicense": 0,
-          "image": "Pumpkin-Spice-Breakfast-Shake-495020.jpg"
+        "id": 495020,
+        "title": "Pumpkin Spice Breakfast Shake",
+        "readyInMinutes": 10,
+        "servings": 1,
+        "sourceUrl": "http://ohmyveggies.com/recipe-pumpkin-spice-breakfast-shake/",
+        "openLicense": 0,
+        "image": "Pumpkin-Spice-Breakfast-Shake-495020.jpg"
         },
         {
-          "id": 694990,
-          "title": "Quick Breakfast Taco",
-          "readyInMinutes": 15,
-          "servings": 1,
-          "sourceUrl": "http://www.eatingwell.com/recipes/quick_breakfast_taco.html",
-          "openLicense": 0,
-          "image": "quick-breakfast-taco-694990.jpg"
+        "id": 694990,
+        "title": "Quick Breakfast Taco",
+        "readyInMinutes": 15,
+        "servings": 1,
+        "sourceUrl": "http://www.eatingwell.com/recipes/quick_breakfast_taco.html",
+        "openLicense": 0,
+        "image": "quick-breakfast-taco-694990.jpg"
         },
         {
-          "id": 505163,
-          "title": "Healthy Gluten-Free Breakfast Sandwich + a Yoga Challenge",
-          "readyInMinutes": 5,
-          "servings": 1,
-          "sourceUrl": "http://www.queenofquinoa.me/2014/01/gluten-free-breakfast-sandwich-yoga-challenge/",
-          "openLicense": 0,
-          "image": "Healthy-Gluten-Free-Breakfast-Sandwich-+-a-Yoga-Challenge-505163.jpg"
+        "id": 505163,
+        "title": "Healthy Gluten-Free Breakfast Sandwich + a Yoga Challenge",
+        "readyInMinutes": 5,
+        "servings": 1,
+        "sourceUrl": "http://www.queenofquinoa.me/2014/01/gluten-free-breakfast-sandwich-yoga-challenge/",
+        "openLicense": 0,
+        "image": "Healthy-Gluten-Free-Breakfast-Sandwich-+-a-Yoga-Challenge-505163.jpg"
         },
         {
-          "id": 569542,
-          "title": "My Favorite Breakfast Smoothie {a.k.a. Super Food Smoothie!}",
-          "readyInMinutes": 45,
-          "servings": 4,
-          "sourceUrl": "http://www.melskitchencafe.com/my-favorite-healthy-breakfast-smoothie-a-k-a-super-food-smoothie/",
-          "openLicense": 0,
-          "image": "My-Favorite-Breakfast-Smoothie-{a-k-a--Super-Food-Smoothie}-569542.jpg"
+        "id": 569542,
+        "title": "My Favorite Breakfast Smoothie {a.k.a. Super Food Smoothie!}",
+        "readyInMinutes": 45,
+        "servings": 4,
+        "sourceUrl": "http://www.melskitchencafe.com/my-favorite-healthy-breakfast-smoothie-a-k-a-super-food-smoothie/",
+        "openLicense": 0,
+        "image": "My-Favorite-Breakfast-Smoothie-{a-k-a--Super-Food-Smoothie}-569542.jpg"
         },
         {
-          "id": 720583,
-          "title": "Quinoa Breakfast Tacos with Kale + Butternut Squash",
-          "readyInMinutes": 25,
-          "servings": 4,
-          "sourceUrl": "http://www.simplyquinoa.com/quinoa-breakfast-tacos-with-kale-butternut-squash/",
-          "openLicense": 0,
-          "image": "quinoa-breakfast-tacos-with-kale-+-butternut-squash-720583.jpg"
+        "id": 720583,
+        "title": "Quinoa Breakfast Tacos with Kale + Butternut Squash",
+        "readyInMinutes": 25,
+        "servings": 4,
+        "sourceUrl": "http://www.simplyquinoa.com/quinoa-breakfast-tacos-with-kale-butternut-squash/",
+        "openLicense": 0,
+        "image": "quinoa-breakfast-tacos-with-kale-+-butternut-squash-720583.jpg"
+        },
+        {
+        "id": 550998,
+        "title": "Salad for breakfast? Oh yeah! Breakfast Quinoa Salad",
+        "readyInMinutes": 45,
+        "servings": 3,
+        "sourceUrl": "http://thehealthyfoodie.com/2012/07/09/salad-for-breakfast-oh-yeah-breakfast-quinoa-salad/",
+        "openLicense": 0,
+        "image": "salad-for-breakfast-oh-yeah-breakfast-quinoa-salad-550998.jpg"
+        },
+        {
+        "id": 626324,
+        "title": "Dr. Oz Keepin’ it Smooth High Protein Breakfast Smoothie",
+        "readyInMinutes": 6,
+        "servings": 2,
+        "sourceUrl": "http://www.blenderbabes.com/lifestyle-diet/dairy-free/dr-oz-keepin-it-smooth-high-protein-breakfast-smoothie-recipes/",
+        "openLicense": 0,
+        "image": "Dr--Oz-Keepin-it-Smooth-High-Protein-Breakfast-Smoothie-626324.jpg"
+        },
+        {
+        "id": 495555,
+        "title": "Mexican Chocolate Breakfast Shake",
+        "readyInMinutes": 10,
+        "servings": 1,
+        "sourceUrl": "http://ohmyveggies.com/recipe-mexican-chocolate-breakfast-shake/",
+        "openLicense": 0,
+        "image": "Mexican-Chocolate-Breakfast-Shake-495555.jpg"
+        },
+        {
+        "id": 676275,
+        "title": "Chocolate Peanut Butter Banana Breakfast Shake",
+        "readyInMinutes": 5,
+        "servings": 2,
+        "sourceUrl": "http://www.cookingclassy.com/2015/02/chocolate-peanut-butter-banana-breakfast-shake/",
+        "openLicense": 0,
+        "image": "Chocolate-Peanut-Butter-Banana-Breakfast-Shake-676275.jpg"
+        },
+        {
+        "id": 678594,
+        "title": "90-Second Pumpkin Pie Breakfast Quinoa",
+        "readyInMinutes": 2,
+        "servings": 1,
+        "sourceUrl": "http://www.simplyquinoa.com/90-second-pumpkin-pie-breakfast-quinoa/",
+        "openLicense": 0,
+        "image": "90-Second-Pumpkin-Pie-Breakfast-Quinoa-678594.jpg"
         }
       ]
     }
@@ -76,7 +121,7 @@ class App extends Component {
     const params = {
       apiKey: API_KEY,
       query: term,
-      number: 5
+      number: 10
     };
 
     axios
