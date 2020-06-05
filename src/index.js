01adf1bf9935a049bb08a5245dc10f6f1eb0eda1
Kyle Stefano <kstefano327@gmail.com>
2020-06-05 10:07:21 -0400

    getting heading set up

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import { BrowserRouter } from 'react-router-dom'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Header from './components/Header';
 import './components/style.css'
+import './components/fonts.css';
+
 ReactDOM.render(
   <BrowserRouter>
+    <Header />
     <App />
   </BrowserRouter>,
   document.getElementById('root')
