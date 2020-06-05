0e847a24d511df9aafb2363460c37abb0369cf69
Spalding vance <51721866+spaldingVance@users.noreply.github.com>
2020-06-05 11:18:20 -0400

    Create SearchBar.js

---
+++ src/components/SearchBar.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+
+  render() {
+    return (
+      <form className="form d-inline shadow-sm">
+        <div className="input-group mb-3 ">
+          <div className="input-group-prepend ">
+            <div className="input-group-btn">
+              <button type="button" className="btn btn-primary search-button shadow-sm" onClick={this.handleClick}>
+                Search
+                  </button>
+                </div>
+              </div>
+              <input className="input-group form-control ml-sm-2 search-bar shadow-sm" value={this.state.term} onChange={this.onInputChange} placeholder="find your recipe"/>
+          </div>
+        </form>
+    );
+  }
+
+  handleClick() {
+    this.props.recipeSearch(this.state.term);
+    this.setState({ term: ''});
+  }
+
+  onInputChange(event) {
+    console.log(event);
+    this.setState({ term: event.target.value });
+  }
+
+}
+
+export default SearchBar;
